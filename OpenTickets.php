4232fc66c9bae8d2207ef1fe8ae2c6a702c0b3f4
Rondal Archer <ronnyarsenal@yahoo.com>
2015-07-06 21:06:34 -0500

    Update OpenTickets.php

--- OpenTickets.php
+++ OpenTickets.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
-
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <link rel="stylesheet"  href="images/SupportTemplate.css" type="text/css" />
 <title>Support Site</title>
-
+</script>
 </head>
-    <body>
+<body>
 <div id="wrap">
   <div id="header">
     <h1 id="logo">Support Site</h1>
@@ -15,7 +14,7 @@
       <ul>
           <li><a href="Announcements.php">Home</a></li>
         <li><a href="SupportPage.php">Support</a></li>
-        <li><a href="logout.php">Sign Out</a></li>
+        <li><a href="logout.php">Log Out</a></li>
         <li><a href="#">Other</a></li>
       </ul>
     </div>
@@ -24,19 +23,21 @@
     <div id="sidebar" >
       <h1 class="clear"></h1>
       <ul class="sidemenu">
-          <p>you are currently logged in as: 
+          <li>you are currently logged in as:
           <?php
            session_start();
            if(isset($_SESSION['username']))
            {  
            $_name = $_SESSION['username'];
-           echo " $_name ";         
-           }
-           ?>         
-          </p>
+           echo "$_name" ;
+           }            
+          ?>
+          </li>
       </ul>
       <ul class="sidemenu">
           <li><a href="user.php">User</a></li>
+        <li><a href="#">Other</a></li>
+        <li><a href="#">Other</a></li>
       </ul>
       <h1>Search</h1>
       <div class="searchform">
@@ -48,34 +49,78 @@
         </form>
       </div>
       <h1></h1>
-      <p></p>
       <p class="align-right"></p>
       <h1></h1>
       <p></p>
     </div>
-    <div id="main"> <a name="TemplateInfo"></a>
+    <div align ='center' id="main"> <a name="TemplateInfo"></a>
       <div class="box">
-        <h1>User</h1>
-        <ul>
-            <li><label><a href=" MyInformation.php">My Information</a></label></li>
-       </ul>
-      </div>
-      <a name="SampleTags"></a>
+          <div ><h1>Open Tickets</h1>
+                  <form action='close.php' method = 'post'>
+            <?php
 
-        </form>
-      </div>
-    </div>
-    <br />
+           $con = mysqli_connect('localhost','root','','tickets'); 
+            if (!$con) 
+            { 
+            die('Could not connect to MySQL: ' . mysqli_error()); 
+            }
+
+
+
+//process query
+$result_set = mysqli_query($con, "SELECT * FROM OpenTickets");
+$num_messages = MySqli_num_rows($result_set);
+?> 
+<?php
+
+echo "<table border =`1` style=background-color:#FFFFF>";
+$num=0;
+        
+While($row = mysqli_fetch_array($result_set))
+{
+$Id = $row['Id'];
+$Title = $row['Title'];
+$Department = $row['Department'];
+$Priority = $row['Priority'];
+$Description = $row['Description'];
+$Email = $row['Email'];
+
+$Date = date ("H:i:s m-d-Y");
+
+
+$num++;
+
+echo "<tr><td>$Id</td>";
+echo "<td>$Title</td>";
+echo "<td>$Description</td>";
+echo "<td>$Department</td>";
+echo "<td>$Priority</td>";
+echo "<td>$Email</td>";
+echo "<td>$Date</td>";
+echo "<td><form role='form' action='close.php' method='post' enctype='multipart/form-data'><input type=\"hidden\" name=\"Id\" value=\"".$row["Id"]."\">";
+echo "<button type='submit' class='btn btn-default'>Close</button></form></td>";
+}
+echo"</form>";
+echo"</Table>";
+$totalopenTickets = $num;
+echo"Total number of open tickets: $totalopenTickets";
+?>
+<a name="SampleTags"></a>
+
   </div>
+          </div>
+      </div>
+
 </div>
 <div id="footer-wrap">
   <div class="footer-left">
    <p class="align-left"></p>
   </div>
   <div class="footer-right">      
-<p></p>
+<p>    
+ <?php $mysqltime = date ("m-d-Y");
+      echo"$mysqltime"?>
+</p>
  </div>
 </div>
-</body>   
-    </body>
 </html>
